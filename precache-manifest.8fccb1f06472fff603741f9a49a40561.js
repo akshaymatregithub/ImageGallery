f1161a9a0f5fa2bf2a20634912e1fb89abe2ddad
akshaymatregithub <akshaymatregithub@gmail.com>
2022-05-21 22:36:06 +0530

    Updates

---
+++ precache-manifest.8fccb1f06472fff603741f9a49a40561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abec76bcb1cffca8ca2b3d71f515404",
+    "url": "/ImageGallery/index.html"
+  },
+  {
+    "revision": "726d4d4b04a3652e15d7",
+    "url": "/ImageGallery/static/css/main.80cfa04c.chunk.css"
+  },
+  {
+    "revision": "04944aa6da86483a59b7",
+    "url": "/ImageGallery/static/js/2.e8456ec3.chunk.js"
+  },
+  {
+    "revision": "ab790d7f4927e929140961c444db2a72",
+    "url": "/ImageGallery/static/js/2.e8456ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726d4d4b04a3652e15d7",
+    "url": "/ImageGallery/static/js/main.ff586946.chunk.js"
+  },
+  {
+    "revision": "c58c9a8236deee5de9ae",
+    "url": "/ImageGallery/static/js/runtime-main.cfbde8f0.js"
+  }
+]);
